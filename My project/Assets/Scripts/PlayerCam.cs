307d3124b693b141295deb723a5fd7b6565cfd3a
DEO6872 <jesustheboss211@gmail.com>
2023-06-08 13:17:14 -0700

    bad fps game that is nowhere near to being finished, janky controls, no look function, camera is lock, not enough code set up

---
+++ My project/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+    public Transform orientation;
+    public float xRotation;
+    public float yRotation;
+    
+    private void start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        
+        yRotation += mouseX;
+
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
